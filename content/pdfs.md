8713f34b2a4b4433c88d7f093183468565222f44
muggleL <ffshine@hotmail.com>
2020-06-09 14:01:27 +0800

    修改

---
+++ content/pdfs.md
@@ -0,0 +1,16 @@
+---
+title: PDFS
+date: '2020-06-08'
+slug: pdfs
+author: DG
+---
+
+- [毕业设计](/PDF/毕业设计.pdf)
+
+- [维基版哀江南赋](/PDF/维基版哀江南赋.pdf)
+
+- [临高启明](/PDF/临高启明.pdf)
+
+- [性别战争](/PDF/性别战争.pdf)
+
+
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 cd /home/ss/git/blog_file
-
+git add --all
+git commit -m $1
 pwd
 hugo --theme=hugo-ivy --baseUrl="https://blog.o0o0o0.de"
 
 # 部署
 cd ../muggleL
-rm -rf css js categories post search tags note daily
+rm -rf css js categories post search tags note daily pdfs
 rm *.html *.xml
 mv ../blog_file/public/* ./
 echo "开始部署"
 git add *
-git commit -m "update"
+git commit -m $1
 git push
