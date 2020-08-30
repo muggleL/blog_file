dd3152d467d38bf1a06f28cfa1d62dcae81cae5b
muggleL <ffshine@hotmail.com>
2020-08-30 13:17:25 +0800

    fix: 修复logo 显示大小👌

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ cd ../muggleL
 rm -rf css js categories post search tags note daily pdfs
 rm *.html *.xml
 mv ../blog_file/public/* ./
-echo "开始部署"
+echo "\n\n开始部署"
 git add *
 git commit -m "$*"
 git push
